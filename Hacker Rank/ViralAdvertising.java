886efdae8c5ca81bfc2686e1f24ca0b32d185c01
1sravan <147555925+1sravan@users.noreply.github.com>
2025-07-20 15:44:59 +0530

    Create ViralAdvertising.java

---
+++ Hacker Rank/ViralAdvertising.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int viralAdvertising(int n) {
+        int likes=0,cumulative=0,first=5;
+           for(int i=1;i<=n;i++){
+               likes = (first/2);
+               cumulative = cumulative + likes;
+               first = likes * 3;
+           }
+           return cumulative;
+
+    }
+
+}
+
+public class ViralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
