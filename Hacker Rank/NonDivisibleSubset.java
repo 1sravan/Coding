7541dff30d577ed4aec46be7129ef965467019ba
1sravan <147555925+1sravan@users.noreply.github.com>
2025-07-21 11:18:11 +0530

    Create NonDivisibleSubset.java

---
+++ Hacker Rank/NonDivisibleSubset.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+ 
+    public static int nonDivisibleSubset(int k, List<Integer> s) {
+        int[] remainderCounts = new int[k];
+        
+        for (int num : s) {
+            remainderCounts[num % k]++;
+        }
+        int count = Math.min(remainderCounts[0], 1);
+
+        for (int i = 1; i <= k / 2; i++) {
+            if (i == k - i) {
+                count += Math.min(remainderCounts[i], 1);
+            } else {
+                count += Math.max(remainderCounts[i], remainderCounts[k - i]);
+            }
+        }
+
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+ 
+        int result = Result.nonDivisibleSubset(k, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
