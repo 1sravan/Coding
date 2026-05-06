c6ca94e622dd469e5225df536e2c4d58df037ae5
1sravan <147555925+1sravan@users.noreply.github.com>
2026-05-06 23:15:14 +0530

    Update 350_intersection_of_two_arrays_II.java

--- LeetCode/350_intersection_of_two_arrays_II.java
+++ LeetCode/350_intersection_of_two_arrays_II.java
@@ -16,5 +16,31 @@ class Solution {
             result[i] = res.get(i);
         }
         return result;
+
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+
+        // int i = 0, j = 0;
+        // List<Integer> res = new ArrayList<>();
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] == nums2[j]) {
+        //         res.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     } 
+        //     else if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } 
+        //     else {
+        //         j++;
+        //     }
+        // }
+
+        // int[] result = new int[res.size()];
+        // for (int k = 0; k < res.size(); k++) {
+        //     result[k] = res.get(k);
+        // }
+
+        // return result;
     }
 }
