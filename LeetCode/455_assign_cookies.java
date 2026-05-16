0e769dcdf6a76ff60be4398a4a37edef46ff9566
1sravan <147555925+1sravan@users.noreply.github.com>
2026-05-17 00:18:58 +0530

    Create 455_assign_cookies.java

---
+++ LeetCode/455_assign_cookies.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int child = 0;
+        for (int cookie = 0;cookie < s.length && child < g.length;cookie++)
+            if (s[cookie] >= g[child])
+                child++;
+        return child;
+    }
+}
