867b18de33d4d6ee7c4a41c581b1813d05183606
1sravan <147555925+1sravan@users.noreply.github.com>
2026-06-05 08:54:09 +0530

    Create 2270_number_of_ways_to_split_array.java

---
+++ LeetCode/2270_number_of_ways_to_split_array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        long leftsum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftsum += nums[i];
+            long rightsum = sum - leftsum;
+
+            if (leftsum >= rightsum) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
