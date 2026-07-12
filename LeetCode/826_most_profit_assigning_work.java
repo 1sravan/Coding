c45d9ad5331d1045a737696cdc4170441f1d5a74
1sravan <147555925+1sravan@users.noreply.github.com>
2026-07-12 23:58:37 +0530

    Create 826_most_profit_assigning_work.java

---
+++ LeetCode/826_most_profit_assigning_work.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+
+        int n = difficulty.length;
+        int[][] jobs = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(worker);
+
+        int j = 0;
+        int maxProfit = 0;
+        int total = 0;
+
+        for (int ability : worker) {
+            while (j < n && jobs[j][0] <= ability) {
+                maxProfit = Math.max(maxProfit, jobs[j][1]);
+                j++;
+            }
+            total += maxProfit;
+        }
+
+        return total;
+    }
+}
