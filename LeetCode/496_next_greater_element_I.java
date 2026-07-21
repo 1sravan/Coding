4c97d93d23c424cac147555b68f8b15e531f59cc
1sravan <147555925+1sravan@users.noreply.github.com>
2026-07-21 23:56:14 +0530

    Update 496_next_greater_element_I.java

--- LeetCode/496_next_greater_element_I.java
+++ LeetCode/496_next_greater_element_I.java
@@ -1,13 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null || nums1.length == 0) return new int[]{};
         HashMap<Integer,Integer> hashMap = new HashMap<>();
-        for(int i=0;i<nums2.length;i++){
-            int j=i+1;
-            while(j < nums2.length && nums2[j] < nums2[i]){
-                j++;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!stack.isEmpty() && nums2[i] > stack.peek()){
+                stack.pop();
             }
-            hashMap.put(nums2[i],(j >= nums2.length)?-1:nums2[j]);
+            hashMap.put(nums2[i],stack.isEmpty()?-1:stack.peek());
+            stack.push(nums2[i]);
         }
+        
+        // for(int i=0;i<nums2.length;i++){
+        //     int j=i+1;
+        //     while(j < nums2.length && nums2[j] < nums2[i]){
+        //         j++;
+        //     }
+        //     hashMap.put(nums2[i],(j >= nums2.length)?-1:nums2[j]);
+        // }
+        
         for(int i=0;i<nums1.length;i++){
             nums1[i]=hashMap.get(nums1[i]);
         }
