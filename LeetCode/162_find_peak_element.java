fd63bf5860ae934cbb651c5094bd5e3b62843f53
1sravan <147555925+1sravan@users.noreply.github.com>
2026-08-14 22:16:36 +0530

    Create 162_find_peak_element.java

---
+++ LeetCode/162_find_peak_element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(mid-1>0 && mid+1>nums.length && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                return mid;
+            else if(mid+1<nums.length && nums[mid]<nums[mid+1]) left=mid+1;
+            else right=mid-1;
+        }
+        return left;
+    }
+}
