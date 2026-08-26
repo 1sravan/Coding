b98250236e1273a6725305674907f237fce4c412
1sravan <147555925+1sravan@users.noreply.github.com>
2026-08-26 23:53:11 +0530

    Create 463_island_perimeter.java

---
+++ LeetCode/463_island_perimeter.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int islands=0,neighbours=0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    ++islands;
+                    if(i-1>=0&&grid[i-1][j]==1)
+                        ++neighbours;
+                    if(j-1>=0&&grid[i][j-1]==1)
+                        ++neighbours;
+                }
+            }
+        }
+     return islands*4-neighbours*2;
+        
+    }
+}
